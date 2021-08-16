0ca9febf4d5bdd51f89d76a71dd04c58547f4ccd
sandycodebrew <sandeepdas@codebrewinnovations.com>
2021-08-16 16:17:11 +0530

    cart update

--- src/Screens/Cart/Cart.js
+++ src/Screens/Cart/Cart.js
@@ -709,10 +709,20 @@ export default function Cart({navigation, route}) {
             })
           : null}
 
-        {item?.isDeliverable ?null:
-        <View style={{marginHorizontal:moderateScale(10)}}>
-         <Text style={{fontSize:moderateScale(12),fontFamily:fontFamily.medium,color:colors.redFireBrick}}>{'The specific items are not deliverable to this address. Please remove the items or change the address '}</Text>
-          </View>}
+        {item?.isDeliverable ? null : (
+          <View style={{marginHorizontal: moderateScale(10)}}>
+            <Text
+              style={{
+                fontSize: moderateScale(12),
+                fontFamily: fontFamily.medium,
+                color: colors.redFireBrick,
+              }}>
+              {
+                'The specific items are not deliverable to this address. Please remove the items or change the address '
+              }
+            </Text>
+          </View>
+        )}
 
         {/* offerview */}
         <TouchableOpacity
