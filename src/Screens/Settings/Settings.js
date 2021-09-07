8d7844a4ebe09c90df08bcd2e014322f9d3ced48
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-09-07 11:51:15 +0530

    ui change for dark theme

--- src/Screens/Settings/Settings.js
+++ src/Screens/Settings/Settings.js
@@ -217,6 +217,23 @@ export default function Settings({route, navigation}) {
 
       <View style={{...commonStyles.headerTopLine}} />
       {/* <KeyboardAwareScrollView bounces={false}> */}
+      <View
+        style={{
+          marginHorizontal: moderateScale(20),
+          marginTop: moderateScaleVertical(20),
+        }}>
+        <Text
+          style={
+            isDarkMode
+              ? [
+                  styles.darkAppearanceTextStyle,
+                  {color: MyDarkTheme.colors.text},
+                ]
+              : styles.darkAppearanceTextStyle
+          }>
+          {strings.APPEARANCE}
+        </Text>
+      </View>
 
       <View
         style={{
@@ -279,7 +296,7 @@ export default function Settings({route, navigation}) {
                 ]
               : styles.darkAppearanceTextStyle
           }>
-          {strings.DARK_APPEARANCE}
+          {strings.AUTOMATIC}
         </Text>
         <ToggleSwitch
           isOn={isOn}
