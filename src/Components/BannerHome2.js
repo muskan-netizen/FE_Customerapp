03160a9505d65e6fd11fab70f32cf1ab0c417c11
Girraj Sharma <girraj.s@codebrewinnovations.com>
2021-09-14 17:00:25 +0530

    Strings updated

--- src/Components/BannerHome2.js
+++ src/Components/BannerHome2.js
@@ -56,11 +56,9 @@ const BannerHome2 = ({
       ? getImageUrl(
           item.image.path.proxy_url,
           item.image.path.image_path,
-          '1400/600',
+          '2000/600',
         )
-      : getImageUrl(item.image.proxy_url, item.image.image_path, '1500/600');
-
-    console.log(imageUrl, 'imageUrl');
+      : getImageUrl(item.image.proxy_url, item.image.image_path, '2000/600');
 
     return (
       <>
