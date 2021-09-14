826692982f29874589599ac35d546f7b789155c6
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-09-14 18:47:29 +0530

    merged with main

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -294,14 +294,11 @@ export default {
   icBrandInActive: require('../assets/images/RoyoorderNewImages/ic_tag.png'),
   icCelebActive: require('../assets/images/RoyoorderNewImages/ic_celeb2.png'),
   icCelebInActive: require('../assets/images/RoyoorderNewImages/ic_celeb1.png'),
-<<<<<<< HEAD
-  icMap: require('../assets/images/RoyoorderNewImages/ic_map.png')
-=======
   icRedPhone: require('../assets/images/RoyoorderNewImages/Group24.png'),
   icLocation1: require('../assets/images/RoyoorderNewImages/ic_location1.png'),
   icRedOval: require('../assets/images/RoyoorderNewImages/ic_oval2.png'),
   icRedChecked: require('../assets/images/RoyoorderNewImages/ic_checked.png'),
   icUnChecked: require('../assets/images/RoyoorderNewImages/ic_unchecked.png'),
   currentLocation: require('../assets/images/RoyoorderNewImages/ic_current.png'),
->>>>>>> main
+  icMap: require('../assets/images/RoyoorderNewImages/ic_map.png'),
 };
