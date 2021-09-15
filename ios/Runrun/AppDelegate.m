e246616fba1195623e1b5c0e804771f6db352323
gulsherkhan <gulsherkhan@codebrewinnovations.com>
2021-09-15 10:46:46 +0530

    conflicts resolved

--- ios/Runrun/AppDelegate.m
+++ ios/Runrun/AppDelegate.m
@@ -39,16 +39,16 @@
   
 
   [RNSplashScreen show];
-  
-    for (NSString* family in [UIFont familyNames])
-    {
-        NSLog(@"%@", family);
-  
-        for (NSString* name in [UIFont fontNamesForFamilyName: family])
-        {
-            NSLog(@"  %@", name);
-        }
-    }
+//  
+//    for (NSString* family in [UIFont familyNames])
+//    {
+//        NSLog(@"%@", family);
+//  
+//        for (NSString* name in [UIFont fontNamesForFamilyName: family])
+//        {
+//            NSLog(@"  %@", name);
+//        }
+//    }
   return YES;
 }
 
