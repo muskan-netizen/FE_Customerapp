500803f98e5c8959ac4eccf562890aefaf734792
gulsherkhan <gulsherkhan@codebrewinnovations.com>
2021-10-07 17:17:48 +0530

    code merged

--- src/Screens/Cart/Cart3.js
+++ src/Screens/Cart/Cart3.js
@@ -555,6 +555,7 @@ export default function Cart({navigation, route}) {
         // systemuser: DeviceInfo.getUniqueId(),
       })
       .then((res) => {
+        console.log("item added++",res)
         actions.cartItemQty({});
         updateState({
           cartItems: [],
