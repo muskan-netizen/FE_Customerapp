20d6dfa0947071b54eae3d5a7dc0e7cb9c69fd3e
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-10-08 20:33:00 +0530

    url update

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
-//export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'http://192.168.100.89:8005/api/v1';
 // export const API_BASE_URL = 'http://192.168.100.89:8005/api/v1';
