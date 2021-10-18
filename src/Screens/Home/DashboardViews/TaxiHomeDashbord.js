6f048cff013877d7bfcb8ad3e842525639bf165c
Arun167 <arun.k@codebrewinnovations.com>
2021-10-18 16:25:47 +0530

    some channges

--- src/Screens/Home/DashboardViews/TaxiHomeDashbord.js
+++ src/Screens/Home/DashboardViews/TaxiHomeDashbord.js
@@ -419,10 +419,9 @@ export default function TaxiHomeDashbord({
           }}
           onPress={() => {
             userData?.auth_token
-              ? moveToNewScreen(
-                  navigationStrings.ADDADDRESS,
-                  appMainData?.categories[0],
-                )
+              ? navigation.navigate(navigationStrings.ADDADDRESS, {
+                  data: appMainData?.categories[0],
+                })
               : navigation.navigate(navigationStrings.LOGIN);
           }}>
           <View
