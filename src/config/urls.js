ab2a72817dee187ce8c43e47c6002d713f3944f4
Girraj Sharma <girraj.s@codebrewinnovations.com>
2021-10-19 11:18:43 +0530

    ProductList3 bug fixed

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
-export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'http://192.168.100.89:8005/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.20:8005/api/v1';
@@ -67,7 +67,9 @@ export const GET_PRODUCTS_BASED_ON_CELEBRITYFILTER =
 export const GET_PRODUCTS_BASED_ON_CELEBRITY = getApiUrl('/celebrityProducts');
 export const PLACE_ORDER = getApiUrl('/place/order');
 export const GET_ORDER_DETAIL = getApiUrl('/order-detail');
-export const GET_ORDER_DETAIL_FOR_BILLING = getApiUrl('/order/orderDetails_for_notification/');
+export const GET_ORDER_DETAIL_FOR_BILLING = getApiUrl(
+  '/order/orderDetails_for_notification/',
+);
 export const GET_ALL_ORDERS = getApiUrl('/orders');
 export const GET_VENDOR_DETAIL = getApiUrl('/vendor/category/list');
 export const SEND_REFFERAL_CODE = getApiUrl('/send/referralcode');
