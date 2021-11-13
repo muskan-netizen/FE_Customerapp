9c015b17881b0477dbe00378a1dc28cbac324ac3
Girraj Sharma <girraj.s@codebrewinnovations.com>
2021-11-13 15:36:11 +0530

    blip assets changed

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -379,4 +379,5 @@ export default {
   settings_icon: require('../assets/images/TowFinder/settings.png'),
   settings_red_icon: require('../assets/images/TowFinder/ic_settings1.png'),
   ic_cameraColored: require('../assets/images/ic_cameraColored.png'),
+  ic_placeholderbeeNew: require('../assets/images/ic_placeholderbeeNew.png'),
 };
