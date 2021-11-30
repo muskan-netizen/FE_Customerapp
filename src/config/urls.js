854e130a8e78d5108f2123e227e45855dfb4ae77
Arun167 <arun.k@codebrewinnovations.com>
2021-11-30 15:53:29 +0530

    klickmate assets updated

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.20:8020/api/v1';
 // export const API_BASE_URL = 'http://192.168.103.73:8006/api/v1';
@@ -9,7 +9,7 @@ export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.23:8005/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.20:8005/api/v1';
 // export const API_BASE_URL = 'http://192.168.100.198:8020/api/v1';
-// export const API_BASE_URL = 'https://tranzit.app/api/v1';
+export const API_BASE_URL = 'https://tranzit.app/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 
