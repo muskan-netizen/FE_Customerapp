282a3c57626daf80da0f721f35f5ca6ef090390f
admin <pavan.s@codebrewinnovations.comgit config --global user.name Pavan Sharma>
2021-12-03 10:40:14 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+//export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.20:8020/api/v1';
 // export const API_BASE_URL = 'http://192.168.103.73:8006/api/v1';
@@ -7,7 +7,7 @@ export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.103.81:8005/api/v1';
 //export const API_BASE_URL = 'https://zuzuclean.alerthire.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.96.23:8005/api/v1';
-//export const API_BASE_URL = 'http://192.168.96.20:8005/api/v1';
+export const API_BASE_URL = 'http://192.168.96.20:8005/api/v1';
 // export const API_BASE_URL = 'http://192.168.100.198:8020/api/v1';
 // export const API_BASE_URL = 'https://tranzit.app/api/v1';
 
