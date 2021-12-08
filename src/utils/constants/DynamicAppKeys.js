9c1383ef91d98b2d680e71622e61145ab1c8e9ca
Arun167 <arun.k@codebrewinnovations.com>
2021-12-08 16:03:18 +0530

    some changes

--- src/utils/constants/DynamicAppKeys.js
+++ src/utils/constants/DynamicAppKeys.js
@@ -688,7 +688,7 @@ const appIds = {
     android: 'com.ored',
   }),
   orderchekout: Platform.select({
-    ios: 'com.customer.orderchekout',
+    ios: 'com.orderchekout.app',
     android: 'com.orderchekout',
   }),
   maxisdelivery: Platform.select({
