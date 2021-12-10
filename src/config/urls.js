bfca295a0db177e82eae372b5f1e29e514776177
Arun167 <arun.k@codebrewinnovations.com>
2021-12-10 11:20:46 +0530

    some chnages

--- src/config/urls.js
+++ src/config/urls.js
@@ -23,9 +23,9 @@
 // export const API_BASE_URL = 'https://cabdeliv.com/api/v1';
 // export const API_BASE_URL = 'https://helpnowrightnow.com/api/v1';
 // export const API_BASE_URL = 'https://orderchekout.com/api/v1';
-// export const API_BASE_URL = 'https://hairstonexpressgo.com/api/v1';
+export const API_BASE_URL = 'https://hairstonexpressgo.com/api/v1';
 // export const API_BASE_URL = 'https://helpnowrightnow.com/api/v1';
-export const API_BASE_URL = 'https://ufood.ca/api/v1';
+// export const API_BASE_URL = 'https://ufood.ca/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 
