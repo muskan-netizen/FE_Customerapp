9298935a394a1a9b77d4414359a7da55640c47d5
Arun167 <arun.k@codebrewinnovations.com>
2021-12-29 11:23:00 +0530

    some changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -34,6 +34,7 @@ export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://order.drivereeshops.com/api/v1';
 // export const API_BASE_URL = 'https://tranzit.app/api/v1';
 // export const API_BASE_URL = 'https://orders.halalhmc.org/api/v1';
+// export const API_BASE_URL = 'https://tranzit.app/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 
