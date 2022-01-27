ca5239d4ea36b5bc6efbbcf90d2fb0c22c18cb29
Gulsher Khan <gulsherkhan@192.168.1.3>
2022-01-27 17:04:48 +0530

    fairex issue fixed

--- src/Screens/Cart/Cart3.js
+++ src/Screens/Cart/Cart3.js
@@ -219,6 +219,9 @@ export default function Cart({ navigation, route }) {
 
   let businessType = appData?.profile?.preferences?.business_type || null;
 
+
+  console.log("cart items",cartItems)
+
   useFocusEffect(
     React.useCallback(() => {
       if (!checkCartItem?.data?.item_count) {
@@ -1634,7 +1637,7 @@ export default function Cart({ navigation, route }) {
 
                               <View
                                 pointerEvents={btnLoader ? 'none' : 'auto'}
-                                style={{ flex: 0.3 }}>
+                                style={{flex:0.3 }}>
                                 <View style={styles.incDecBtnContainer}>
                                   <TouchableOpacity
                                     style={{ alignItems: 'center' }}
@@ -1648,7 +1651,7 @@ export default function Cart({ navigation, route }) {
                                   <View
                                     style={{
                                       alignItems: 'center',
-                                      width: moderateScale(20),
+                                      // width: moderateScale(20),
                                       height: moderateScale(20),
                                       justifyContent: 'center',
                                     }}>
