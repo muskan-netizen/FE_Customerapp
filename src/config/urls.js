3bc4755f19b4ebff771ac26ae2ebd823289c2035
Arun167 <arun.k@codebrewinnovations.com>
2022-01-28 11:54:53 +0530

    live url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://runrun.ae/api/v1';
 
@@ -50,7 +50,7 @@
 // export const API_BASE_URL = 'https://cannabusxpress.com/api/v1';
 // export const API_BASE_URL = 'https://test.taquick.com/api/v1';
 // export const API_BASE_URL = 'https://yeboy.mx/api/v1';
-export const API_BASE_URL = 'https://nadeliver.com/api/v1';
+// export const API_BASE_URL = 'https://nadeliver.com/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 export const SEND_OTP = getApiUrl('/auth/sendOtp');
