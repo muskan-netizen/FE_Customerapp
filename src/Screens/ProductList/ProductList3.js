f27f85a55ecd0f92e1609748c4ebbfbfb3c211c3
pavan sharma <pavan.s@codebrewinnovations.com>
2022-02-28 12:29:56 +0530

    bug fixes

--- src/Screens/ProductList/ProductList3.js
+++ src/Screens/ProductList/ProductList3.js
@@ -2411,8 +2411,8 @@ export default function Products({route, navigation}) {
                         opacity: 1,
                         fontSize: textScale(10),
                       }}>
-                      {checkEvenOdd(categoryInfo.timeofLineOfSightDistance)}-
-                      {checkEvenOdd(categoryInfo.timeofLineOfSightDistance + 5)}
+                      ~{checkEvenOdd(categoryInfo.timeofLineOfSightDistance)}
+                      {/* - {checkEvenOdd(categoryInfo.timeofLineOfSightDistance + 5)} */}
                     </Text>
                   ) : null}
                 </View>
