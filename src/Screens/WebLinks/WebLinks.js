324574378fb8a72ef7412a3d0ba702d77ac5c3b4
Gulsher Khan <gulsherkhan@Gulshers-MacBook-Air.local>
2022-03-04 18:04:22 +0530

    bug fixes

--- src/Screens/WebLinks/WebLinks.js
+++ src/Screens/WebLinks/WebLinks.js
@@ -901,30 +901,6 @@ export default function WebLinks({navigation, route}) {
       </WrapperContainer>
     );
   }
-  const _getLocationFromParams = () => {
-    if (
-      paramData?.details &&
-      paramData?.details?.formatted_address != location?.address
-    ) {
-      const address = paramData?.details?.formatted_address;
-      const res = {
-        address: address,
-        latitude: paramData?.details?.geometry?.location.lat,
-        longitude: paramData?.details?.geometry?.location.lng,
-      };
-      updateState({
-        address:address
-      })
-     
-    }
-  };
-
-  useEffect(() => {
-    _getLocationFromParams();
-    // if (addressSearch) {
-    //   _getLocationFromParams();
-    // }
-  }, [paramData?.details]);
 
   return (
     <WrapperContainer
