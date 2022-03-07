48c2f82b58f454eef0699c70f5847f8c8b7c84f0
pavan sharma <pavan.s@codebrewinnovations.com>
2022-03-07 17:08:09 +0530

    zendesk theme issue fixes

--- src/Screens/Account/Account3.js
+++ src/Screens/Account/Account3.js
@@ -35,7 +35,7 @@ import {
   getRandomColor,
 } from '../../utils/helperFunctions';
 import stylesFun from './styles';
-import ZendeskChat from '../../library/react-native-zendesk-chat';
+import ZendeskChat from 'react-native-zendesk-chat';
 import Share from 'react-native-share';
 import {appIds} from '../../utils/constants/DynamicAppKeys';
 import DeviceInfo from 'react-native-device-info';
