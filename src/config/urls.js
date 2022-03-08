11262c4d7c8faec4f49d0e3326595067d1521218
Gulsher Khan <gulsherkhan@Gulshers-MacBook-Air.local>
2022-03-08 18:35:08 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://sales.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://sales.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://hmc.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
@@ -6,7 +6,7 @@ export const API_BASE_URL = 'https://sales.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://2d24-103-72-170-243.ngrok.io/api/v1';
 // export const API_BASE_URL = 'https://releezer.shop/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1';
-// export const API_BASE_URL = 'http://192.168.97.160:9000/api/v1';//inderjit
+export const API_BASE_URL = 'http://192.168.97.160:9000/api/v1';//inderjit
 
 // export const API_BASE_URL = 'https://961a-112-196-88-218.ngrok.io/api/v1'; // preet
 // export const API_BASE_URL = 'http://192.168.97.123:8020/api/v1'; // sujata
