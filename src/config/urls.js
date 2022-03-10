6009b59716dd28834f0c63a812f2bc68209edf0c
Girraj Sharma <girraj.s@codebrewinnovations.com>
2022-03-10 12:15:08 +0530

    url changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,8 +1,8 @@
-export const API_BASE_URL = 'https://sales.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://sales.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://hmc.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://2d24-103-72-170-243.ngrok.io/api/v1';
 // export const API_BASE_URL = 'https://releezer.shop/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1';
@@ -243,4 +243,6 @@ export const USER_REGISTRATION_DOCUMENT = getApiUrl(
   '/user/registration/document',
 );
 export const GET_USER_PROFILE = getApiUrl('/getProfile');
-export const VENDOR_PRODUCTS_OPTIMIZE_FILTERS = getApiUrl('/vendor/vendorProductsFilterOptimize');
+export const VENDOR_PRODUCTS_OPTIMIZE_FILTERS = getApiUrl(
+  '/vendor/vendorProductsFilterOptimize',
+);
