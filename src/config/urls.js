4338f4f1776925ee36be577f58c159a0908d5122
admin <admin@admins-iMac.local>
2022-03-21 19:00:22 +0530

    Targets

--- src/config/urls.js
+++ src/config/urls.js
@@ -3,7 +3,7 @@
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://9303-112-196-88-218.ngrok.io/api/v1';
-// export const API_BASE_URL = 'https://releezer.shop/api/v1';
+export const API_BASE_URL = 'https://releezer.shop/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.97.160:9090/api/v1';//inderjit
 
@@ -55,7 +55,7 @@
 // export const API_BASE_URL = 'https://marketplace.verofax.com/api/v1';
 // export const API_BASE_URL = 'https://order.loopwhole.ca/api/v1';
 // export const API_BASE_URL = 'https://elcheregio.mx/api/v1';
-export const API_BASE_URL = 'https://donepacked.com/api/v1';
+// export const API_BASE_URL = 'https://donepacked.com/api/v1';
 // export const API_BASE_URL = 'https://cannabusxpress.com/api/v1';
 // export const API_BASE_URL = 'https://order-dev.taquick.com/api/v1';
 // export const API_BASE_URL = 'https://yeboy.mx/api/v1';
