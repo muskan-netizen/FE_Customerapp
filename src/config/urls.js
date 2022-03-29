f6d3bd56a67b2cf697e0c310c2c4dec377bdcde5
Girraj Sharma <girraj.s@codebrewinnovations.com>
2022-03-29 15:38:15 +0530

    working on kyc

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,11 +1,12 @@
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
+// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'https://2d24-103-72-170-243.ngrok.io/api/v1';
 // export const API_BASE_URL = 'https://releezer.shop/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1';
-// export const API_BASE_URL = 'http://192.168.96.127:8005/api/v1';
+export const API_BASE_URL = 'http://192.168.97.192:8005/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 export const SEND_OTP = getApiUrl('/auth/sendOtp');
@@ -192,3 +193,5 @@ export const VENDOR_PRODUCTS_OPTIMIZE_FILTERS = getApiUrl(
 export const GET_PRODUCT_FAQS = getApiUrl('/products_faq');
 export const UPDATE_PRODUCT_FAQS_CART = getApiUrl('/cart/productfaq/update');
 export const RATE_TO_DRIVER = getApiUrl('/rating/update-driver-rating');
+export const GET_CATEGORY_KYC_DOCUMENT = getApiUrl('/category_kyc_document');
+export const SUBMIT_CATEGORY_KYC = getApiUrl('/submit_category_kyc');
