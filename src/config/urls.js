dc94511e6553744881cd19308002b7cfc1d724fa
admin <admin@admins-iMac-4.local>
2022-04-05 12:54:31 +0530

    assets and name changed rideShare and newyork mini mart

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,13 +1,18 @@
 // export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'https://2d24-103-72-170-243.ngrok.io/api/v1';
 // export const API_BASE_URL = 'https://releezer.shop/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1';
 // export const API_BASE_URL = 'https://meateasy.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://order.veg-hub.com/api/v1';
+// export const API_BASE_URL = 'https://seratho.com/api/v1';
+// export const API_BASE_URL = 'https://bezaliohub.com/api/v1';
+
+// export const API_BASE_URL = 'https://food-order.konga.com/api/v1';
+
 
 
 
@@ -18,7 +23,7 @@
 // export const API_BASE_URL = 'https://hemptify.com/api/v1';
 // export const API_BASE_URL = 'https://sirvu.ca/api/v1';
 // export const API_BASE_URL = 'https://empirehub.com/api/v1';
-export const API_BASE_URL = 'https://ullaz.co.ke/api/v1';
+// export const API_BASE_URL = 'https://ullaz.co.ke/api/v1';
 // export const API_BASE_URL = 'https://localdropoff.info/api/v1';
 // export const API_BASE_URL = 'https://onscart.com/api/v1';
 
