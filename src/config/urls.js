866f4a43c565c6219a01a9a14e53ccf308a2bd77
Arun167 <arun.k@codebrewinnovations.com>
2022-04-06 12:59:59 +0530

    build changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -70,11 +70,11 @@
 // export const API_BASE_URL = 'https://agrionline.ph/api/v1';
 // export const API_BASE_URL = 'https://marketplace.verofax.com/api/v1';
 // export const API_BASE_URL = 'https://order.loopwhole.ca/api/v1';
-// export const API_BASE_URL = 'https://elcheregio.mx/api/v1';
+export const API_BASE_URL = 'https://elcheregio.mx/api/v1';
 //export const API_BASE_URL = 'https://donepacked.com/api/v1';
 // export const API_BASE_URL = 'https://cannabusxpress.com/api/v1';
 // export const API_BASE_URL = 'https://order-dev.taquick.com/api/v1';
-export const API_BASE_URL = 'https://yeboy.mx/api/v1';
+// export const API_BASE_URL = 'https://yeboy.mx/api/v1';
 // export const API_BASE_URL = 'https://nadeliver.com/api/v1';
 // export const API_BASE_URL = 'https://samakeemart.com/api/v1';
 // export const API_BASE_URL = 'https://ritenowdelivery.com/api/v1';
