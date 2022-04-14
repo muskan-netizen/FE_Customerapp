34ed83bec88c251fddd8d280f0b1647b5352a17e
admin <admin@admins-iMac-4.local>
2022-04-14 18:12:36 +0530

    Taking pull

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'http://192.168.97.192:8005/api/v1';
 // export const API_BASE_URL = 'https://releezer.shop/api/v1';
