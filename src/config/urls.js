fb7eb3e731f4a0b6e4a6153d6efab37e4d930ab3
sandycodebrew <sandeepdas@codebrewinnovations.com>
2022-04-15 10:46:54 +0530

    Ola assets changed in android

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,14 +1,11 @@
 // export const API_BASE_URL = 'https://donepacked.com/api/v1';
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
-export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
-
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 
 //Vishwas
 // export const API_BASE_URL = 'http://192.168.100.185:8080/api/v1';
-
-
 
 //owais nor
 // export const API_BASE_URL ='http://d1cc-112-196-88-218.ngrok.io/api/v1';
