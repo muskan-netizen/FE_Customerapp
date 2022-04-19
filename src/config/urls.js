156b1163dd74a2fc293fc2bc10af2485c3f0d409
Gulsher Khan <gulsherkhan@Gulshers-MacBook-Air.local>
2022-04-19 17:56:57 +0530

    bug fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 export const SEND_OTP = getApiUrl('/auth/sendOtp');
