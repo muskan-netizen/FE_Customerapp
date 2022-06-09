079f28d9070ef52d7915f1940f12c6f170e32126
CBL-Mac_038 <cbl-mac_038@CBL-Mac-038s-iMac-Pro.local>
2022-06-09 17:22:57 +0530

    bug fixes

--- src/Screens/ProductList/ProductList3.js
+++ src/Screens/ProductList/ProductList3.js
@@ -264,7 +264,7 @@ export default function Products({ route, navigation }) {
         />
       </View>
     );
-  }, [cloneSectionList, btnLoader, productListId, repeatItems, cartId])
+  }, [cloneSectionList, btnLoader, productListId, repeatItems, cartId,categoryInfo])
 
 
   const renderSectionHeader = useCallback((props) => {
@@ -360,7 +360,7 @@ export default function Products({ route, navigation }) {
         <View style={styles.horizontalLine} />
       </View>
     );
-  }, [productListData, btnLoader, productListId, repeatItems, cartId])
+  }, [productListData, btnLoader, productListId, repeatItems, cartId,categoryInfo])
 
 
 
