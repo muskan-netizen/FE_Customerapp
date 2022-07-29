d4ea7b4005fd723b29df1ce3c414e9505662679a
CBL-Mac-030 <cbl-mac-030@CBL-Mac-030s-iMac-Pro.local>
2022-07-29 11:45:13 +0530

    Changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -11,7 +11,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://swiffy.royoorders.com/api/v1'
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
- export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+ //export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = ' https://36kel.com.ph/api/v1'
 
@@ -49,7 +49,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://butlerdeliveryusa.com/api/v1'
 
 // export const API_BASE_URL = 'https://admin.shoorafresh.com.au/api/v1'
-//
+ export const API_BASE_URL ='https://hitchdelivery.ie/api/v1'
 //Vishwas
 // export const API_BASE_URL = 'http://192.168.99.124:8000/api/v1';
 // export const API_BASE_URL = 'http://192.168.100.185:8080/api/v1';
