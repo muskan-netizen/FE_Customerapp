dd377683f0bb07de17066b619939f1edf1dfd5ce
CBL-Mac-006 <cbl-mac-006@CBL-Mac-006s-iMac-Pro.local>
2022-08-03 13:23:56 +0530

    Update profile phone number crash fixes

--- src/Components/PhoneNumberInputWithUnderline.js
+++ src/Components/PhoneNumberInputWithUnderline.js
@@ -20,6 +20,8 @@ import {
 import {TextInput} from 'react-native-paper';
 import {useDarkMode} from 'react-native-dark-mode';
 import {MyDarkTheme} from '../styles/theme';
+import {getBundleId} from 'react-native-device-info';
+import {appIds} from '../utils/constants/DynamicAppKeys';
 
 const PhoneNumberInputWithUnderline = ({
   cca2 = '',
