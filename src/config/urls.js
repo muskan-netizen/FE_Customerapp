76a0502d398824c7a076c3b441eb65aa0cb9e761
CBL-Mac-049 <cbl-mac-049@CBL-Mac-049s-iMac-Pro.local>
2022-09-16 17:45:01 +0530

    code merge with dev_girraj

--- src/config/urls.js
+++ src/config/urls.js
@@ -33,7 +33,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://hokitch.com/api/v1';
 
 // blacnetwork
-// export const API_BASE_URL = 'https://app.blacnetwork.com/api/v1';
+export const API_BASE_URL = 'https://app.blacnetwork.com/api/v1';
 
 // hemptify
 // export const API_BASE_URL = 'https://hemptify.com/api/v1';
@@ -218,7 +218,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://canupickmeup.com/api/v1';
 
 // export const API_BASE_URL = 'https://hmc.royoorders.com/api/v1';
-export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
+// export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://hemptify.com/api/v1';
 // export const API_BASE_URL = 'https://sirvu.ca/api/v1';
