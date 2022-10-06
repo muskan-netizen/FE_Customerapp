cf60b2a723fc5d0ba7dc4530a8324ecfd10fa101
pavan sharma <pavanpancholi8@gmail.com>
2022-10-06 18:03:23 +0530

    product order from open button changed

--- src/Screens/ProductList/ProductList3.js
+++ src/Screens/ProductList/ProductList3.js
@@ -1483,50 +1483,6 @@ export default function Products({route, navigation}) {
     }
   };
 
-  //  to load  catagerios by A-z
-  // const onAtoZFilter  =  () => {
-  //   try {
-  //     let allFilterData = cloneDeep(allFilters);
-  //     var newData = [];
-  //     var variants = [];
-  //     var options = [];
-
-  //     var allSelectedVariantOptionsPairs = allFilterData
-  //         .filter((i) => i?.id != -1 && i?.id != -2)
-  //         .map((itm, inx) => {
-  //             return itm?.value;
-  //         })
-  //         .map((j, jnx) => {
-  //             if (j.length) return j.filter((x) => x?.value?.selected);
-  //         })
-  //         .filter((final) => final?.length)
-  //         .map((finalArray, finalIndex) => {
-  //             finalArray?.map((z, znx) => {
-  //                 newData?.push(z);
-  //             });
-  //             return finalArray;
-  //         });
-
-  //     if (newData.length) {
-  //         newData.map((i) => {
-  //             variants.push(i?.variant_type_id);
-  //             options.push(i?.id);
-  //         });
-  //         allSelectedVariantOptionsPairs = newData;
-  //     }
-
-  //     let filterData = {
-  //         selectedSorting: 'a_to_z',
-  //         selectedVariants: variants,
-  //         selectedOptions: options,
-  //         sleectdBrands: []
-  //     }
-  //     onFilterApply(filterData)
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
   useEffect(() => {
     getAllVendorFilters();
   }, []);
--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
-//export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL ='https://sales.focushires.com/api/v1';
 
 // export const API_BASE_URL = 'http://192.168.96.166:9091/api/v1';
@@ -15,7 +15,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://mobimarket.com.mx/api/v1'
 // export const API_BASE_URL = 'https://swiffy.royoorders.com/api/v1'
 // export const API_BASE_URL = 'https://tasmeem.royoorders.com/api/v1';
- export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
+ //export const API_BASE_URL = 'https://sales.royoorders.com/api/v1'; //sales demo
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 
 // export const API_BASE_URL = 'https://perucabs.com/api/v1';
@@ -553,4 +553,8 @@ export const VENDOR_CHAT = '/api/room/fetchRoomByVendor';
 export const SEND_MESSAGE = '/api/chat/sendMessageJoin';
 export const GET_ALL_MESSAGES = '/api/chat';
 export const ALL_ROOM_USER = '/api/chat/getRoomUser';
+
+
+
+// static drop-location
 export const STATIC_DROP_LOCATIONS = getApiUrl('/static-dropoff-locations');
