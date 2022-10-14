4084ecf4d776752bfe69fe4b3774016d9babb016
arun.k <arun.k@codebrewinnovations.com>
2022-10-14 18:30:25 +0530

    bug fixes

--- src/Screens/ShortCode/ShortCode.js
+++ src/Screens/ShortCode/ShortCode.js
@@ -2615,14 +2615,14 @@ export default function ShortCode({route, navigation}) {
 
     if (!!res?.primary_language?.id) {
       header = {
-        code: '4a7329',
-        // code: shortCode,
+        // code: '4a7329',
+        code: shortCode,
         language: res?.primary_language?.id,
       };
     } else {
       header = {
-        code: '4a7329',
-        // code: shortCode,
+        // code: '4a7329',
+        code: shortCode,
       };
     }
     console.log(header, 'header*******');
