865316bc56848219a0de4b66b68ebd5f064899dd
pavan sharma <pavanpancholi8@gmail.com>
2022-11-16 12:23:19 +0530

    chat screen bug fixes

--- src/Screens/ChatScreen/ChatScreen.js
+++ src/Screens/ChatScreen/ChatScreen.js
@@ -66,8 +66,8 @@ export default function ChatScreen({ route, navigation }) {
     isLoading: false,
     roomUsers: [],
     isVoiceRecord: false,
-    allRoomUsersAppartFromAgent:[],
-    allAgentIds:[]
+    allRoomUsersAppartFromAgent: [],
+    allAgentIds: [],
   });
   const {
     isLoading,
@@ -76,7 +76,7 @@ export default function ChatScreen({ route, navigation }) {
     showParticipant,
     chatUsersData,
     allRoomUsersAppartFromAgent,
-    allAgentIds
+    allAgentIds,
   } = state;
 
   const updateState = (data) => setState((state) => ({ ...state, ...data }));
@@ -153,28 +153,24 @@ export default function ChatScreen({ route, navigation }) {
           language: languages?.primary_language?.id,
         }
       );
-      console.log("fetchAllRoomUser res", res);
-      if (!!res?.userData && isFocused) {
- 
-    
 
-       const allRoomUsersAppartFromAgentAry= res?.userData.splice(res?.userData.findIndex(item => item?.user_type != "agent") )
-       const allAgentIdsAry= res?.userData.splice(res?.userData.findIndex(item => item?.user_type == "agent") )
-       console.log(allRoomUsersAppartFromAgentAry,allAgentIdsAry,"allChatUseresallChatUseres");
-       
-       updateState({
-        allRoomUsersAppartFromAgent:allRoomUsersAppartFromAgentAry,
-        allAgentIds:allAgentIdsAry,
+      if (!!res?.userData && isFocused) {
+        const allRoomUsersAppartFromAgent = res?.userData.filter(function (el) {
+          return el.user_type != "agent";
+        });
+        const allAgentIds = res?.userData.filter(function (el) {
+          return el.user_type == "agent";
+        });
+        updateState({
+          allRoomUsersAppartFromAgent: allRoomUsersAppartFromAgent,
+          allAgentIds: allAgentIds,
           roomUsers: res?.userData,
         });
-       
       }
     } catch (error) {
       console.log("error raised in fetchAllRoomUser api", error);
     }
   }, [allRoomUsersAppartFromAgent, allAgentIds, roomUsers]);
-
-  
 
   const checkToMessage = () => {
     let userType = paramData?.type;
@@ -192,67 +188,67 @@ export default function ChatScreen({ route, navigation }) {
     }
   };
 
-  const onSend = useCallback(async (messages = []) => {
-    if (String(messages[0].text).trim().length < 1) {
-      return;
-    }
-    let phoneNumber = !!userData.phone_number
-      ? `+${userData?.dial_code} ${userData.phone_number}`
-      : null;
-    console.log("phoneNumberphoneNumber", userData);
-    let userImage = !!userData?.source
-      ? getImageUrl(
-          userData?.source?.proxy_url,
-          userData?.source?.image_path,
-          "200/200"
-        )
-      : null;
-      
+  const onSend = useCallback(
+    async (messages = []) => {
+      if (String(messages[0].text).trim().length < 1) {
+        return;
+      }
+      let phoneNumber = !!userData.phone_number
+        ? `+${userData?.dial_code} ${userData.phone_number}`
+        : null;
+      console.log("phoneNumberphoneNumber", userData);
+      let userImage = !!userData?.source
+        ? getImageUrl(
+            userData?.source?.proxy_url,
+            userData?.source?.image_path,
+            "200/200"
+          )
+        : null;
 
-    try {
-      const apiData = {
-        room_id: paramData?._id,
-        message: messages[0].text,
-        user_type: !!userData?.is_superadmin ? "admin" : "user",
-        to_message: checkToMessage(),
-        from_message: !!userData?.is_superadmin ? "from_admin" : "from_user",
-        user_id: userData?.id,
-        email: userData.email,
-        username: userData?.name,
-        phone_num: phoneNumber,
-        display_image: userImage,
-        sub_domain: "192.168.101.88", //this is static value
-        //'room_name' =>$data->name,
-        chat_type: paramData?.type,
-      };
-      console.log("apiDataapiData", apiData);
-      const res = await actions.sendMessage(apiData, {
-        code: appData?.profile?.code,
-        currency: currencies?.primary_currency?.id,
-        language: languages?.primary_language?.id,
-      });
-      console.log("on send message res", res);
-      socketServices.emit("save-message", res);
-      // const message = {
-      //   _id: userData.id,
-      //   auth_user_id: userData.id,
-      //   message: messages[0].text,
-      //   createdAt: new Date(),
-      //   username: userData?.name,
-      //   display_image: getImageUrl(
-      //     userData?.source?.proxy_url,
-      //     userData?.source?.image_path,
-      //     '200/200',
-      //   )
-      // };
-      await sendToUserNotification(paramData?._id, messages[0].text);
-      // setMessages(previousMessages => GiftedChat.append(previousMessages, message))
-    } catch (error) {
-      console.log("error raised in fetchAllMessages api", error);
-    }
-  }, [allRoomUsersAppartFromAgent, allAgentIds]);
-
-
+      try {
+        const apiData = {
+          room_id: paramData?._id,
+          message: messages[0].text,
+          user_type: !!userData?.is_superadmin ? "admin" : "user",
+          to_message: checkToMessage(),
+          from_message: !!userData?.is_superadmin ? "from_admin" : "from_user",
+          user_id: userData?.id,
+          email: userData.email,
+          username: userData?.name,
+          phone_num: phoneNumber,
+          display_image: userImage,
+          sub_domain: "192.168.101.88", //this is static value
+          //'room_name' =>$data->name,
+          chat_type: paramData?.type,
+        };
+        console.log("apiDataapiData", apiData);
+        const res = await actions.sendMessage(apiData, {
+          code: appData?.profile?.code,
+          currency: currencies?.primary_currency?.id,
+          language: languages?.primary_language?.id,
+        });
+        console.log("on send message res", res);
+        socketServices.emit("save-message", res);
+        // const message = {
+        //   _id: userData.id,
+        //   auth_user_id: userData.id,
+        //   message: messages[0].text,
+        //   createdAt: new Date(),
+        //   username: userData?.name,
+        //   display_image: getImageUrl(
+        //     userData?.source?.proxy_url,
+        //     userData?.source?.image_path,
+        //     '200/200',
+        //   )
+        // };
+        await sendToUserNotification(paramData?._id, messages[0].text);
+        // setMessages(previousMessages => GiftedChat.append(previousMessages, message))
+      } catch (error) {
+        console.log("error raised in fetchAllMessages api", error);
+      }
+    },
+    [allRoomUsersAppartFromAgent, allAgentIds]
+  );
 
   const sendToUserNotification = (id, text) => {
     let apiData = {
@@ -263,23 +259,26 @@ export default function ChatScreen({ route, navigation }) {
       chat_type: paramData?.type,
       order_id: paramData?.order_id,
       all_agentids: allAgentIds,
-      order_vendor_id:paramData?.order_vendor_id,
-      username:userData?.name,
-      vendor_id:paramData?.vendor_id,
-      auth_id:userData?.id
-    };    
-   console.log(apiData,"apiDataapiDataapiData");
-    actions.sendNotification(apiData, {
-      code: appData?.profile?.code,
-      currency: currencies?.primary_currency?.id,
-      language: languages?.primary_language?.id,
-    }).then((res)=>{
-      console.log(res,"response+++++",apiData);
-    }).catch((error)=>{
-      console.log(error,"errororr in notification");
-    })
+      order_vendor_id: paramData?.order_vendor_id,
+      username: userData?.name,
+      vendor_id: paramData?.vendor_id,
+      auth_id: userData?.id,
+    };
+    console.log(apiData, "apiDataapiDataapiData");
+    actions
+      .sendNotification(apiData, {
+        code: appData?.profile?.code,
+        currency: currencies?.primary_currency?.id,
+        language: languages?.primary_language?.id,
+      })
+      .then((res) => {
+        console.log(res, "response+++++", apiData);
+      })
+      .catch((error) => {
+        console.log(error, "errororr in notification");
+      });
   };
-  
+
   const showRoomUser = useCallback(
     (props) => {
       if (_.isEmpty(roomUsers)) {
--- src/Screens/ShortCode/ShortCode.js
+++ src/Screens/ShortCode/ShortCode.js
@@ -2804,13 +2804,13 @@ export default function ShortCode({route, navigation}) {
 
     if (!!res?.primary_language?.id) {
       header = {
-     // code: '2d98b5',
-        code: shortCode,
+      //code: 'f34c51',
+       code: shortCode,
         language: res?.primary_language?.id,
       };
     } else {
       header = {
-      // code: '2d98b5',
+     // code: 'f34c51',
        code: shortCode,
       };
     }
