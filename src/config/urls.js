1d81b5a14cd2036691640f71e0603b34fba10c1f
CBL-MAC-016 <cbl-mac-016@CBL-MAC-016s-Mac-mini.local>
2022-11-29 16:24:36 +0530

    builds

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://classicab.org/api/v1';
 // export const API_BASE_URL = 'https://order.mrveloz.com.mx/api/v1';
 // export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
@@ -20,7 +20,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 //export const API_BASE_URL = 'http://192.168.101.140:9090/api/v1';
 //  export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
-export const API_BASE_URL = 'https://ipickndrop.com/api/v1';
+// export const API_BASE_URL = 'https://ipickndrop.com/api/v1';
 // export const API_BASE_URL = 'https://hellodeliver.in/api/v1';
 // export const API_BASE_URL = 'https://handy-pickup.com/api/v1';
 // export const API_BASE_URL = 'https://goadeliv.com/api/v1';
@@ -119,7 +119,7 @@ export const API_BASE_URL = 'https://ipickndrop.com/api/v1';
 // export const API_BASE_URL = 'https://tmgsalesllc.com/api/v1';
 // export const API_BASE_URL = 'https://fijieats.com/api/v1'
 // export const API_BASE_URL = 'https://swantaxi.net/api/v1'
-// export const API_BASE_URL = 'https://keystonedelivery.com/api/v1'
+// export const API_BASE_URL = 'https://keystonedelivery.com/api/v1';
 // export const API_BASE_URL = 'https://meateasy.royoorders.com/api/v1'
 // export const API_BASE_URL = 'https://aaujaaunepal.com/api/v1'
 
