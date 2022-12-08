e36da538a43b9e5c745722ad765c85dc2ffda4ba
CBL-MAC-016 <cbl-mac-016@CBL-MAC-016s-Mac-mini.local>
2022-12-08 12:29:05 +0530

    builds

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,11 +1,11 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
-export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://speedy.com.do/api/v1';
 // export const API_BASE_URL = 'https://shophairrun.app/api/v1';
 // export const API_BASE_URL = 'https://nittosadai.com/api/v1';
 // export const API_BASE_URL = 'https://classicab.org/api/v1';
 // export const API_BASE_URL = 'https://order.mrveloz.com.mx/api/v1';
-// export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://nittosadai.com/api/v1';
 // export const API_BASE_URL = 'https://laundrozone.net/api/v1';
 // export const API_BASE_URL = 'http://192.168.100.130:9090/api/v1';
