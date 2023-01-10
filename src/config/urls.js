819472adf8a8b3d6fe2812eecadc2f274aa8b47b
CBL-MAC-034 <cbl-mac-034@CBL-MAC-034s-iMac-Pro.local>
2023-01-10 19:30:51 +0530

    Assets

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL='https://uber-weeds.com/api/v1'
 // export const API_BASE_URL='https://pas41.net/api/v1'
-// export const API_BASE_URL='https://carryfood.org/api/v1'
+export const API_BASE_URL='https://carryfood.org/api/v1'
 // export const API_BASE_URL='http://192.168.101.54:9090/api/v1'
 // export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://nittosadai.com/api/v1';
@@ -144,7 +144,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // lastminutedresss
 // export const API_BASE_URL = 'https://lastminutedress.com/api/v1';
 //Vishwas
-export const API_BASE_URL = 'https://speedy.com.do/api/v1';
+// export const API_BASE_URL = 'https://speedy.com.do/api/v1';
 // export const API_BASE_URL='https://pas41.net/api/v1'
 // export const API_BASE_URL = 'https://hitaxi.taxi/api/v1';
 // export const API_BASE_URL = 'https://pideunfleety.com/api/v1';
