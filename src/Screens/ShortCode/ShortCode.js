7c070ada74f492e96733f9a1247020dd2de8e4c0
pavan sharma <pavanpancholi8@gmail.com>
2023-02-01 11:43:25 +0530

    toc build upload

--- src/Screens/ShortCode/ShortCode.js
+++ src/Screens/ShortCode/ShortCode.js
@@ -3156,7 +3156,6 @@ export default function ShortCode({ route, navigation }) {
 
     actions
       .initApp({}, header, false, null, null, true)
-
       .then((res) => {
         console.log("header response--->", res);
         if (res.data.mobile_banners.length > 0) {
--- src/config/urls.js
+++ src/config/urls.js
@@ -12,10 +12,10 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://classicab.org/api/v1';
 // export const API_BASE_URL = 'https://order.mrveloz.com.mx/api/v1';
 //export const API_BASE_URL = 'https://carryfood.org/api/v1';
- export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
+//  export const API_BASE_URL = 'https://api.royoorders.com/api/v1';
 // export const API_BASE_URL = 'https://nittosadai.com/api/v1';
 // export const API_BASE_URL = 'https://laundrozone.net/api/v1';
-// export const API_BASE_URL = 'http://192.168.100.130:9090/api/v1';
+//export const API_BASE_URL = 'http://192.168.102.70:8004/api/v1';
 // export const API_BASE_URL = 'https://app.bendeta.biz/api/v1';
 // export const API_BASE_URL = 'https://mealtime.menu/api/v1';
 // export const API_BASE_URL='https://jeevannapp.com/api/v1'
@@ -396,6 +396,10 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL = 'https://dev.zonesso.com/api/v1';
 // export const API_BASE_URL = 'http://192.168.101.214:9002/api/v1';
 //export const API_BASE_URL = 'https://order.clickokart.com/api/v1'
+
+//tdc order
+export const API_BASE_URL = 'https://tdcrider.rostaging.com/api/v1'
+
 
 export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 
