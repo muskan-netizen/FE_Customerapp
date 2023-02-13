a4cb537f0879381a5917c07cf3ae4c959509e2e7
Gulsher Khan <gulsherkhan@Gulshers-MacBook-Air.local>
2023-02-13 11:00:22 +0530

    minor issue fixes

--- src/Screens/ShortCode/getAppCode.ts
+++ src/Screens/ShortCode/getAppCode.ts
@@ -3,6 +3,7 @@ import { appIds, shortCodes } from "../../utils/constants/DynamicAppKeys";
 
 export const getAppCode = () => {
     switch (getBundleId()) {
+        case appIds.royoorder: return shortCodes.royoorder;
         case appIds.tranzit: return shortCodes.tranzit
         case appIds.runrun: return shortCodes.runrun;
         case appIds.hmoobhub: return shortCodes.hmoobhub;
