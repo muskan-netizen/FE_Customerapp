b2202ce5e5e89613b9e44e40181e542a618ebc2b
Girraj Sharma <girrajsharma@Girrajs-MacBook-Air.local>
2023-02-23 17:11:48 +0530

    latest code

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -136,9 +136,9 @@ export default function Home({ route, navigation }) {
   const { profile } = appData;
 
   useEffect(() => {
-    if (!!userData?.auth_token && !!appData?.profile?.socket_url) {
-      socketServices.initializeSocket(appData?.profile?.socket_url);
-    }
+    // if (!!userData?.auth_token && !!appData?.profile?.socket_url) {
+    //   socketServices.initializeSocket(appData?.profile?.socket_url);
+    // }
   }, [appData]);
 
   useFocusEffect(
