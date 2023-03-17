c00c2e9f7d793c1d99ba0dbc4a7c608eba9a340f
MonikaSangar <sangarmonika82@gmail.com>
2023-03-17 16:03:26 +0530

    keys add

--- src/Screens/Cart/parts/Footer.js
+++ src/Screens/Cart/parts/Footer.js
@@ -23,14 +23,14 @@ import {
 } from '../../../styles/responsiveSize';
 /**
 * SwipeableSection Part
-* @param {ideleteItem,addDeleteCartItems,tokenConverterPlusCurrencyNumberFormater,showTaxFeeArea,selectedTipAmount,userData,scheduleType,isDarkMode,colors,styles,FastImage,imagePath,fontFamily,appIds,codMinAmount,selectedPayment,digit_after_decimal,additional_preferences,MyDarkTheme,currencies,cartData,placeLoader,ButtonComponent,localeSheduledOrderDate,_selectTime,placeOrder,strings,selectedTipvalue,_onGiftBoxSelection,themeColors,isGiftBoxSelected,hitSlopProp, setAppSessionRedirection, updateState,selectedTip,setSelectedTipAmount,setInstruction,clearSceduleDate} props 
+* @param {ideleteItem,addDeleteCartItems,tokenConverterPlusCurrencyNumberFormater,showTaxFeeArea,selectedTipAmount,userData,scheduleType,isDarkMode,colors,styles,FastImage,imagePath,fontFamily,appIds,codMinAmount,selectedPayment,digit_after_decimal,additional_preferences,MyDarkTheme,currencies,cartData,placeLoader,ButtonComponent,localeSheduledOrderDate,_selectTime,placeOrder,strings,selectedTipvalue,_onGiftBoxSelection,themeColors,isGiftBoxSelected,hitSlopProp, setAppSessionRedirection, updateState,selectedTip,setSelectedTipAmount,setInstruction,clearSceduleDate,_selectTimeLaundry,laundrySelectedPickupDate,laundrySelectedDropOffDate,laundrySelectedPickupSlot,laundrySelectedDropOffSlot,pickupDriverComment,setPickupDriverComment,dropOffDriverComment,setDropOffDriverComment,vendorComment,setVendorComment} props 
 * @returns 
 */
 
 
 function Footer(props) {
 
-  const { instruction, preferences, tokenConverterPlusCurrencyNumberFormater, showTaxFeeArea, selectedTipAmount, userData, scheduleType, isDarkMode, colors, styles, FastImage, imagePath, fontFamily, appIds, codMinAmount, selectedPayment, digit_after_decimal, additional_preferences, MyDarkTheme, currencies, cartData, strings, businessType, localeDropOffDate, appData, placeLoader, ButtonComponent, _selectTime, localeSheduledOrderDate, placeOrder, selectedTipvalue, _onGiftBoxSelection, themeColors, isGiftBoxSelected, hitSlopProp, setAppSessionRedirection, updateState,selectedTip,setInstruction,setSelectedTipAmount,clearSceduleDate } = props;
+  const { instruction, preferences, tokenConverterPlusCurrencyNumberFormater, showTaxFeeArea, selectedTipAmount, userData, scheduleType, isDarkMode, colors, styles, FastImage, imagePath, fontFamily, appIds, codMinAmount, selectedPayment, digit_after_decimal, additional_preferences, MyDarkTheme, currencies, cartData, strings, businessType, localeDropOffDate, appData, placeLoader, ButtonComponent, _selectTime, localeSheduledOrderDate, placeOrder, selectedTipvalue, _onGiftBoxSelection, themeColors, isGiftBoxSelected, hitSlopProp, setAppSessionRedirection, updateState,selectedTip,setInstruction,setSelectedTipAmount,clearSceduleDate ,_selectTimeLaundry,laundrySelectedPickupDate,laundrySelectedDropOffDate,laundrySelectedPickupSlot,laundrySelectedDropOffSlot,pickupDriverComment,setPickupDriverComment,dropOffDriverComment,setDropOffDriverComment,vendorComment,setVendorComment} = props;
   return (
     <View style={{}}>
       {!!cartData?.category_kyc_count && !!userData?.auth_token && (
