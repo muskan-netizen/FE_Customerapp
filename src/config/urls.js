6dfb88dcd0f75c61e000c75a76a33485306fbd4b
Girraj Sharma <girrajsharma@Girrajs-MacBook-Air.local>
2023-03-30 14:41:25 +0530

    url changes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,12 +1,12 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
 // export const API_BASE_URL='https://hattafoodhub.com/api/v1'
-//  export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://app.bendeta.biz/api/v1';
 // export const API_BASE_URL='https://vduonline.co.in/api/v1'
 // export const API_BASE_URL = 'http://192.168.103.164:8000/api/v1';
 
 // export const API_BASE_URL = 'https://ihelp.app.br/api/v1';
-export const API_BASE_URL = 'http://192.168.102.70:8006/api/v1';
+// export const API_BASE_URL = 'http://192.168.102.70:8006/api/v1';
 // export const API_BASE_URL = 'https://speedy.com.do/api/v1';
 // export const API_BASE_URL = 'https://kaypee.app//api/v1';
 // export const API_BASE_URL = 'https://jeevannapp.com/api/v1';
