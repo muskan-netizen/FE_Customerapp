824c64b54c33d11857c89a8e2e181e7abd08542b
Girraj Sharma <girrajsharma@Girrajs-MacBook-Air.local>
2023-04-19 11:41:19 +0530

    console removed from VendorDetail3

--- src/Screens/VendorDetail/VendorDetail3.js
+++ src/Screens/VendorDetail/VendorDetail3.js
@@ -33,7 +33,6 @@ enableFreeze(true);
 
 export default function VendorDetail3({ navigation, route }) {
   let vendorParams = route?.params?.data;
-  console.log('vendor params', vendorParams);
   const theme = useSelector((state) => state?.initBoot?.themeColor);
   // alert("312")
   const toggleTheme = useSelector((state) => state?.initBoot?.themeToggle);
