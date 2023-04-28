788d13af16ad58a2108dc4a38b1876676e8f8208
Girraj Sharma <girrajsharma@Girrajs-MacBook-Air.local>
2023-04-28 17:22:16 +0530

    tempcorner freelancer mode

--- src/Screens/ShortCode/ShortCode.tsx
+++ src/Screens/ShortCode/ShortCode.tsx
@@ -36,9 +36,7 @@ export default function ShortCode() {
   const initApiHit = async () => {
     const lang = await getItem('setPrimaryLanguage');
     const prevCode = await getItem('saveShortCode');
-    // const appCode = !!prevCode ? prevCode : getAppCode();
-    const appCode = '245bae';
- 
+    const appCode = !!prevCode ? prevCode : getAppCode();
 
     let header = {};
     if (!!lang?.primary_language?.id) {
