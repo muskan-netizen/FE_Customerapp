a5e679b803ef32a73dbf13323c1077c202e8b000
CBL-Mac-006 <cbl-mac-006@CBL-Mac-006s-iMac-Pro.local>
2023-06-01 11:49:29 +0530

    url file change

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 
 // export const API_BASE_URL = 'https://api.royoorders.com/api/v1';  //Live
 // export const API_BASE_URL='http://192.168.102.171:8000/api/v1'
-// export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
+export const API_BASE_URL = 'https://api.rostaging.com/api/v1';
 // export const API_BASE_URL = 'https://api.focushires.com/api/v1'; //Dev
 
 // export const API_BASE_URL = 'https://test.tempcorner.com/api/v1'
@@ -304,7 +304,7 @@ export const SOCKET_URL = 'https://chat.royoorders.com';
 
 // dindDongDelivers
 // export const API_BASE_URL = 'https://dingdongdelivers.com/api/v1'
-export const API_BASE_URL = 'https://punnet.royoorders.com/api/v1'
+// export const API_BASE_URL = 'https://punnet.royoorders.com/api/v1'
 //Dlvrd
 // export const API_BASE_URL = 'https://36kel.com.ph/api/v1';
 
