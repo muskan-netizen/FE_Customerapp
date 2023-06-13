793629e13f6a738c623c3e22af41ab1be0b0a266
Admin <admin@Admins-Mac-mini.local>
2023-06-13 17:15:26 +0530

    taxidashboard darkmode icon bug fixed

--- src/Screens/Home/DashboardViews/TaxiHomeDashbord.js
+++ src/Screens/Home/DashboardViews/TaxiHomeDashbord.js
@@ -357,7 +357,7 @@ export default function TaxiHomeDashbord({
 
   const _renderItem = useCallback(({ item }) => {
     return (
-      <TaxiHomeCategoryCard data={item} onPress={() => continueWithNaxtScreen(item)} mainViewStyle={{ backgroundColor: isDarkMode ? MyDarkTheme.colors.background : colors.backgroundGrey }} />
+      <TaxiHomeCategoryCard data={item} onPress={() => continueWithNaxtScreen(item)} />
     );
   }, [appMainData?.categories || []])
 
