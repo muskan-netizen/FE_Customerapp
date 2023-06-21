8ca44068b0af99509d3b3a8e86d064bf3c1e1074
Gulsher Khan <gulsherkhan@192.168.1.3>
2023-06-21 23:24:07 +0530

    added only demos flvour & targets

--- src/Screens/ShortCode/ShortCode.tsx
+++ src/Screens/ShortCode/ShortCode.tsx
@@ -37,7 +37,7 @@ export default function ShortCode() {
     const lang = await getItem('setPrimaryLanguage');
     const prevCode = await getItem('saveShortCode');
     // const appCode = !!prevCode ? prevCode : getAppCode();
-    const appCode = '245bae'
+    const appCode = '2f3120'
    
     let header = {};
     if (!!lang?.primary_language?.id) {
