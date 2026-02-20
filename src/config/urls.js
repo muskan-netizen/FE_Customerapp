53c317935a1c7b4e5e0bd8f1b922c6b0731575f0
Pallavi Saini <pallavisaini@Pallavis-MacBook-Air-2.local>
2026-02-20 17:41:57 +0530

    firebase setup

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 export const SOCKET_URL = 'https://chat.royoorders.com';
 
 
-export const API_BASE_URL = 'https://restocare.netsolutionindia.com/api/v1';
+export const API_BASE_URL = 'https://restocare.in/api/v1';
 
 
 
